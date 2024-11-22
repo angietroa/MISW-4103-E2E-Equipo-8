8feea9e4571bba7669b2bc6d18ac3c4909177524
Daniela Suárez <cdanisuarez@users.noreply.github.com>
2024-11-21 19:51:38 -0500

    Agrega E067, E068 y E069

---
+++ data-generation/cypress/e2e/E069.cy.js
@@ -0,0 +1,39 @@
+const { faker, fa } = require("@faker-js/faker");
+const LoginPage = require("../pages/login");
+const TagPage = require("../pages/tag");
+
+describe("E069 - Crear un tag con color hexadecimal (aleatorio)", () => {
+  const loginPage = new LoginPage(cy);
+  const tagPage = new TagPage(cy);
+
+  it("Crear un tag con color hexadecimal (aleatorio)", async () => {
+    //Ingresar a la página
+    loginPage.visitPage();
+    loginPage.signInPage();
+
+    //Ir al modulo de tags
+    tagPage.clickOnTagMenu();
+
+    //Dar click en nuevo tag
+    tagPage.clickOnNewTag();
+
+    //Obtener los datos a priori
+    const tagName = faker.lorem.word({ length: { min: 1, max: 191 } });
+    const tagColor = faker.color.rgb().replace('#', '');
+
+    //Ingresar el nombre del tag
+    tagPage.setTagName(tagName);
+
+    //Ingresar el color del tag
+    tagPage.setTagColor(tagColor);
+
+    //Guardar tag
+    tagPage.clickOnSaveTag();
+
+    //Ir al modulo de tags
+    tagPage.clickOnTagMenu();
+
+    //Encontrar tag creado
+    tagPage.findTagNameCreated(tagName);
+  });
+});
