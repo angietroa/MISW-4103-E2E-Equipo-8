1ecbba03f0707cce576875dca206edc5b285fc8f
Esteban Heredia <e.herediar@uniandes.edu.co>
2024-11-27 17:39:36 -0500

    carga de tests a-priori

---
+++ final-strategy/cypress/e2e/E039.cy.js
@@ -0,0 +1,25 @@
+const LoginPage = require("../pages/login");
+const PageObj = require("../pages/page");
+const PAGE_DATAPOOL_APRIORI = require("../data-a-priori/page.json");
+
+describe("E039 - Crear página con botón con caracter vacio a-priori", () => {
+  const data = PAGE_DATAPOOL_APRIORI[28];
+  it("E039 - Crear página con botón con caracter vacio a-priori", async () => {
+    const loginPage = new LoginPage(cy);
+    const page = new PageObj(cy);
+    // Visitar página e iniciar sesión
+    loginPage.visitPage();
+    loginPage.signInPage();
+    // Crear página
+    page.goToPageAndCreate();
+    // Cargar título
+    page.pageTitle(data.title);
+    // Agrear elemento
+    page.addPageElement("Button");
+    // Cargar contenido a botón
+    page.setContentToButton(data.empty_strings);
+    // Publicar página
+    page.publishPage();
+    page.closePublishPopup();
+  });
+});
