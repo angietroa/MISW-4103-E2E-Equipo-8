15444ad7cfc2b15d18cb45114f608adcc7da3084
Esteban Heredia <e.herediar@uniandes.edu.co>
2024-11-29 11:48:11 -0500

    cambios en descripción

--- final-strategy/cypress/e2e/E115.cy.js
+++ final-strategy/cypress/e2e/E115.cy.js
@@ -2,9 +2,9 @@ const LoginPage = require("../pages/login");
 const ConfigPage = require("../pages/config");
 const PAGE_DATAPOOL_APRIORI = require("../data-a-priori/page.json");
 
-describe("E113 - Editar título de metadata con 10 palabras sin espacio a-priori", () => {
+describe("E113 - Editar descripción de metadata con 10 palabras con espacio a-priori", () => {
   const data = PAGE_DATAPOOL_APRIORI[15];
-  it("E113 - Editar título de metadata con 10 palabras sin espacio a-priori", async () => {
+  it("E113 - Editar descripción de metadata con 10 palabras con espacio a-priori", async () => {
     const loginPage = new LoginPage(cy);
     const configPage = new ConfigPage(cy);
 
